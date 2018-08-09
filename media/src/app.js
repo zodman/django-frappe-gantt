f92bbd3c3fa65630a2b0f1b6c6f49b6d1a77d0d9
Your Name <you@example.com>
2018-08-08 20:44:42 -0500

    enhanced

--- media/src/app.js
+++ media/src/app.js
@@ -1,10 +1,23 @@
 var Gantt_ = require("frappe-gantt").default;
 var gantt;
 
+
+toggle_class = function($this){
+// toggle class
+    var elemtns = document.querySelectorAll(".change")
+    for(i=0; i<elemtns.length; i++){
+    	elemtns[i].classList.remove("is-active")
+    }
+    
+    $this.parentNode.classList.add("is-active")
+
+}
+
 window.change = function ($this) {
 	//$this.preventDefault();
     gantt.hide_popup();
     gantt.change_view_mode($this.textContent);
+	toggle_class($this);
 
 }
 
@@ -12,5 +25,8 @@ window.change = function ($this) {
 document.addEventListener('DOMContentLoaded', function(){
  gantt = new Gantt_( document.querySelector("#gantt"), tasks);
  gantt.change_view_mode();
+ var e = document.querySelector(".change a")
+ toggle_class(e);
 });
+
 
